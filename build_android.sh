849fb97329feed375d39abcfd0839f86a03c22f2
hazytint <hazytint@gmail.com>
2020-12-24 02:23:41 +0800

    update build script

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mkdir -p /app/opencv-build
-cd /app/opencv-build
-python ../opencv/platforms/android/build_sdk.py --config=ndk-17.config.py
+#cp build_android.sh PATH_OPENCV_SRC
+#docker run -rm -v PATH_OPENCV_SRC:/app/opencv
+
+mkdir -p /app/opencv/build-android
+cd /app/opencv/build-android
+python ../platforms/android/build_sdk.py --config=ndk-17.config.py
